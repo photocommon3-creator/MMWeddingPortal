506185b8bedbc3a3aa594eee395e3869d0aa15d0
photocommon3-creator <photocommon3@gmail.com>
2025-10-07 23:33:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const wordCloudContainer = document.getElementById('wordCloudContainer');
   
   const canvas = document.createElement('canvas');
-  canvas.width = 600;
+  canvas.width = 400;
   canvas.height = 400;
   wordCloudContainer.appendChild(canvas);
 
@@ -27,8 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
       // Draw word cloud
       WordCloud(canvas, {
         list: wordArray,
-        gridSize: 3,
-        weightFactor: 2,
+        gridSize: 12,
+        weightFactor: 3,
         fontFamily: 'Times, serif',
          color: function(word, weight) {
     const palette = ['#d94f6f', '#f7c59f', '#f1c40f']; // blush pink, soft pink, gold
@@ -87,6 +87,7 @@ $(document).ready(function(){
     showSlide(currentIndex);
   }, 10000);
 });
+
 
 
 
